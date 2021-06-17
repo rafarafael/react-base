975ca8079c90cd761c1bd40a761627cbbc9256f8
Rafael Leite <rl.oliveira321@gmail.com>
2021-06-17 03:10:25 -0300

    instalação do redux

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Container } from '../../styles/GlobalStyles';
 import { Title, Paragrafo } from './styled';
 
 export default function Login() {
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+
+    dispatch({
+      type: 'BOTAO_CLICADO',
+    });
+  }
+
   return (
     <Container>
       <Title isRed={false}>
@@ -11,7 +22,9 @@ export default function Login() {
         <small>Oie</small>
       </Title>
       <Paragrafo>Lorem ipsum</Paragrafo>
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
